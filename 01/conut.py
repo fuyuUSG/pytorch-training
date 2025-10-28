90209fffb4408b55fd4d2e0734df7c84575d67c6
fuyuUSG <234328264+fuyuUSG@users.noreply.github.com>
2025-10-29 01:12:48 +0900

    test commit

---
+++ 01/conut.py
@@ -0,0 +1,6 @@
+def count_word(s, "w"):
+    count = 
+    return count
+
+if __name__ == "__main__":
+    result = count_word(s, "e")
---
+++ 01/main.py
@@ -0,0 +1,7 @@
+from utils.count.py import count_word
+
+if __name__ == "__main__":
+    s = "hello world"
+    print("e:", count_word(s, "e"))
+    print("o:", count_word(s, "o"))
+    print("l:",)
