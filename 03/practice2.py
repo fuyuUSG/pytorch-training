d4abad1bee9cecc264241e87a95ae37d31c5157d
fuyuUSG <234328264+fuyuUSG@users.noreply.github.com>
2025-11-05 09:39:06 +0900

    test commit

---
+++ 03/practice2.py
@@ -0,0 +1,14 @@
+import torch
+from torch import nn
+
+if __name__ == "__main__":
+    _in = torch.ones((32, 1024))
+    print(f"_in: {_in.shape}")
+
+    fc = nn.Linear(in_features=1024, out_features=256, bias=True)
+    out = fc(_in)
+    print(f"practice1: {out.shape}")
+
+    fc2 = nn.Linear(in_features=1024, out_features=2048, bias=True)
+    out2 = fc2(_in)
+    print(f"practice2: {out2.shape}")
